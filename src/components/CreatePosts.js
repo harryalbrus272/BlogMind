8cc6305406096b731fa1f94412a95d892c08b839
Shashwat Kumar Singh <73544122+harryalbrus272@users.noreply.github.com>
2021-07-08 00:15:59 +0530

    make icon buttons and state of the text

--- src/components/CreatePosts.js
+++ src/components/CreatePosts.js
@@ -1,14 +1,172 @@
-import React from "react";
+import React, { useState } from "react";
 import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
+import Highlight from "@tiptap/extension-highlight";
+import Typography from "@tiptap/extension-typography";
+import { Button, Icon, Container } from "semantic-ui-react";
 
 const CreatePosts = () => {
+  const [state, setState] = useState("Hello World! The next is coming up");
+  console.log({ state });
   const editor = useEditor({
-    extensions: [StarterKit],
-    content: "<p>Hello World! 🌎️</p>",
+    extensions: [StarterKit, Highlight, Typography],
+    content: state,
+    onUpdate({ editor }) {
+      setState(editor.getHTML());
+    },
   });
 
-  return <div><EditorContent editor={editor} /></div>;
+  const MenuBar = ({ editor }) => {
+    if (!editor) {
+      return null;
+    }
+    return (
+      <React.Fragment>
+        <Button
+          icon
+          onClick={() => editor.chain().focus().toggleBold().run()}
+          className={editor.isActive("bold") ? "is-active" : ""}
+          size="mini"
+          basic
+        >
+          <Icon name="bold" />
+        </Button>
+        <Button
+          icon
+          onClick={() => editor.chain().focus().toggleItalic().run()}
+          className={editor.isActive("italic") ? "is-active" : ""}
+          size="mini"
+          basic
+        >
+          <Icon name="italic" />
+        </Button>
+        <Button
+          icon
+          onClick={() => editor.chain().focus().toggleStrike().run()}
+          className={editor.isActive("strike") ? "is-active" : ""}
+          size="mini"
+          basic
+        >
+          <Icon name="strikethrough" />
+        </Button>
+        <Button
+          icon
+          onClick={() => editor.chain().focus().toggleCode().run()}
+          className={editor.isActive("code") ? "is-active" : ""}
+          size="mini"
+          basic
+        >
+          <Icon name="code" />
+        </Button>
+        <Button
+          icon
+          onClick={() => editor.chain().focus().setParagraph().run()}
+          className={editor.isActive("paragraph") ? "is-active" : ""}
+          size="mini"
+          basic
+        >
+          <Icon name="paragraph" />
+        </Button>
+        <Button
+          icon
+          onClick={() =>
+            editor.chain().focus().toggleHeading({ level: 1 }).run()
+          }
+          className={
+            editor.isActive("heading", { level: 1 }) ? "is-active" : ""
+          }
+          size="mini"
+          basic
+        >
+          <Icon name="heading" />
+          1
+        </Button>
+        <Button
+          icon
+          onClick={() =>
+            editor.chain().focus().toggleHeading({ level: 2 }).run()
+          }
+          className={
+            editor.isActive("heading", { level: 2 }) ? "is-active" : ""
+          }
+          size="mini"
+          basic
+        >
+          <Icon name="heading" />
+          2
+        </Button>
+        <Button
+          icon
+          onClick={() =>
+            editor.chain().focus().toggleHeading({ level: 3 }).run()
+          }
+          className={
+            editor.isActive("heading", { level: 3 }) ? "is-active" : ""
+          }
+          size="mini"
+          basic
+        >
+          <Icon name="heading" />
+          3
+        </Button>
+        <Button
+          icon
+          onClick={() => editor.chain().focus().toggleBulletList().run()}
+          className={editor.isActive("bulletList") ? "is-active" : ""}
+          size="mini"
+          basic
+        >
+          <Icon name="list ul" />
+        </Button>
+        <Button
+          icon
+          onClick={() => editor.chain().focus().toggleOrderedList().run()}
+          className={editor.isActive("orderedList") ? "is-active" : ""}
+          size="mini"
+          basic
+        >
+          <Icon name="list ol" />
+        </Button>
+        <Button
+          icon
+          onClick={() => editor.chain().focus().toggleBlockquote().run()}
+          className={editor.isActive("blockquote") ? "is-active" : ""}
+          size="mini"
+          basic
+        >
+          <Icon name="quote left" />
+        </Button>
+        <Button
+          icon
+          onClick={() => editor.chain().focus().undo().run()}
+          size="mini"
+          basic
+        >
+          <Icon name="undo" />
+        </Button>
+        <Button
+          icon
+          onClick={() => editor.chain().focus().redo().run()}
+          size="mini"
+          basic
+        >
+          <Icon name="redo" />
+        </Button>
+      </React.Fragment>
+    );
+  };
+
+  return (
+    <div>
+      <Container>
+        <MenuBar editor={editor} />
+        <EditorContent
+          editor={editor}
+          onChange={(e) => setState(e.target.value)}
+        />
+      </Container>
+    </div>
+  );
 };
 
 export default CreatePosts;
