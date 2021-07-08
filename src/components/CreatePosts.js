226c2f2d2853bc9e2da680b2ae39de2474f5f8ea
Shashwat Kumar Singh <73544122+harryalbrus272@users.noreply.github.com>
2021-07-08 14:35:22 +0530

    reset function handled

--- src/components/CreatePosts.js
+++ src/components/CreatePosts.js
@@ -4,12 +4,12 @@ import StarterKit from "@tiptap/starter-kit";
 import Highlight from "@tiptap/extension-highlight";
 import Typography from "@tiptap/extension-typography";
 import TextAlign from "@tiptap/extension-text-align";
-import { Container, Input, Grid } from "semantic-ui-react";
+import { Container, Input, Grid, Button } from "semantic-ui-react";
 import MenuBar from "./MenuBar";
 
 const CreatePosts = () => {
-  const [state, setState] = useState("");
-  console.log({ state });
+  const [content, setContent] = useState("");
+  console.log({ content });
   const editor = useEditor({
     extensions: [
       StarterKit,
@@ -19,11 +19,17 @@ const CreatePosts = () => {
         types: ["heading", "paragraph"],
       }),
     ],
-    content: state,
+    content: content,
     onUpdate({ editor }) {
-      setState(editor.getHTML());
+      setContent(editor.getHTML());
     },
   });
+
+  const handleReset = (e) => {
+    editor.commands.setContent("");
+    editor.commands.focus();
+    setContent("");
+  };
 
   return (
     <div>
@@ -42,6 +48,7 @@ const CreatePosts = () => {
         <MenuBar editor={editor} />
         <Grid
           columns={16}
+          container
           style={{
             margin: "10px 0 0 0",
             border: "1px solid rgba(34,36,38,.15)",
@@ -50,7 +57,6 @@ const CreatePosts = () => {
         >
           <EditorContent
             editor={editor}
-            onChange={(e) => setState(e.target.value)}
             style={{
               minHeight: "400px",
               width: "100%",
@@ -58,6 +64,11 @@ const CreatePosts = () => {
             }}
           />
         </Grid>
+        <Button.Group>
+          <Button onClick={(e) => handleReset(e)}>Reset</Button>
+          <Button.Or />
+          <Button positive>Create Post</Button>
+        </Button.Group>
       </Container>
     </div>
   );
