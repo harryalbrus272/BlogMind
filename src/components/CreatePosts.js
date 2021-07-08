959613001989f07b5683acd75fd2cddb28f61541
Shashwat Kumar Singh <73544122+harryalbrus272@users.noreply.github.com>
2021-07-08 12:46:58 +0530

    UI completed for the text editor

--- src/components/CreatePosts.js
+++ src/components/CreatePosts.js
@@ -4,11 +4,11 @@ import StarterKit from "@tiptap/starter-kit";
 import Highlight from "@tiptap/extension-highlight";
 import Typography from "@tiptap/extension-typography";
 import TextAlign from "@tiptap/extension-text-align";
-import { Container } from "semantic-ui-react";
+import { Container, Input, Grid } from "semantic-ui-react";
 import MenuBar from "./MenuBar";
 
 const CreatePosts = () => {
-  const [state, setState] = useState("Hello World! The next is coming up");
+  const [state, setState] = useState("");
   console.log({ state });
   const editor = useEditor({
     extensions: [
@@ -27,12 +27,37 @@ const CreatePosts = () => {
 
   return (
     <div>
-      <Container>
-        <MenuBar editor={editor} />
-        <EditorContent
-          editor={editor}
-          onChange={(e) => setState(e.target.value)}
+      <Container
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+        }}
+      >
+        <Input
+          fluid
+          placeholder="Enter Blog Title...."
+          style={{ marginBottom: "15px" }}
         />
+        <MenuBar editor={editor} />
+        <Grid
+          columns={16}
+          style={{
+            margin: "10px 0 0 0",
+            border: "1px solid rgba(34,36,38,.15)",
+            borderRadius: ".28571429rem",
+          }}
+        >
+          <EditorContent
+            editor={editor}
+            onChange={(e) => setState(e.target.value)}
+            style={{
+              minHeight: "400px",
+              width: "100%",
+              caretColor: "red",
+            }}
+          />
+        </Grid>
       </Container>
     </div>
   );
