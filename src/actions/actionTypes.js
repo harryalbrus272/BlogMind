eaca48722bdae2a2c759c940d20a184dc884d988
Shashwat Kumar Singh <73544122+harryalbrus272@users.noreply.github.com>
2021-07-08 23:22:23 +0530

    established actions types in the app

---
+++ src/actions/actionTypes.js
@@ -0,0 +1,14 @@
+//Loading list on the home page
+export const LIST_LOAD_START = 'LIST_LOAD_START';
+export const LIST_LOAD_SUCCESS = 'LIST_LOAD_SUCCESS';
+export const LIST_LOAD_FAILED = 'LIST_LOAD_FAILED';
+
+//Loading the specific blog
+export const BLOG_LOAD_START = 'BLOG_LOAD_START';
+export const BLOG_LOAD_SUCCESS = 'BLOG_LOAD_SUCCESS';
+export const BLOG_LOAD_FAILED = 'BLOG_LOAD_FAILED';
+
+//Loading the specific blog
+export const CREATE_POST_START = 'CREATE_POST_START';
+export const CREATE_POST_SUCCESS = 'CREATE_POST_SUCCESS';
+export const CREATE_POST_FAILED = 'CREATE_POST_FAILED';
